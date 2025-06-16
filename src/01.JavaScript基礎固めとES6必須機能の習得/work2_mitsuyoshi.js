30b035df5f368c602a84c350b11b410bac08ccd5
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2025-06-16 16:26:54 +0900

    2 done

--- src/01.JavaScript基礎固めとES6必須機能の習得/work2_mitsuyoshi.js
+++ src/01.JavaScript基礎固めとES6必須機能の習得/work2_mitsuyoshi.js
@@ -3,18 +3,36 @@
 
 const userProfile = {
     basic: {
-      name: '山田太郎',
-      age: 25,
-      email: 'yamada@example.com'
+        name: "山田太郎",
+        age: 25,
+        email: "yamada@example.com",
     },
     address: {
-      prefecture: '東京都',
-      city: '渋谷区'
+        prefecture: "東京都",
+        city: "渋谷区",
     },
-    skills: ['JavaScript', 'Python', 'SQL']
-  };
-  
-  // TODO 1: 分割代入を使ってname, emailを取り出す
-  // TODO 2: スプレッド構文を使ってskillsに'TypeScript'を追加した新配列を作成
-  // TODO 3: userProfileをコピーしてageを26に更新（イミュータブルに）
-  
+    skills: ["JavaScript", "Python", "SQL"],
+};
+
+// TODO 1: 分割代入を使ってname, emailを取り出す
+console.log("===1===\n");
+const {
+    basic: { name, email },
+} = userProfile;
+console.log(`name: ${name}, email: ${email}`);
+
+// TODO 2: スプレッド構文を使ってskillsに'TypeScript'を追加した新配列を作成
+console.log("\n===2===\n");
+const newUserProfile = {
+    ...userProfile,
+    skills: [...userProfile.skills, "TypeScript"],
+};
+console.log(newUserProfile);
+
+// TODO 3: userProfileをコピーしてageを26に更新（イミュータブルに）
+console.log("\n===3===\n");
+const immutable = {
+    ...userProfile,
+    basic: { ...userProfile.basic, age: 26 },
+};
+console.log(immutable);
