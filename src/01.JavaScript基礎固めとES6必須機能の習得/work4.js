d86400426ce855db2743af43e9e8161d27f0a962
otsuki <rena141209.2002@gmail.com>
2025-06-19 12:14:09 +0900

    work4の課題完了

--- src/01.JavaScript基礎固めとES6必須機能の習得/work4.js
+++ src/01.JavaScript基礎固めとES6必須機能の習得/work4.js
@@ -13,23 +13,84 @@
 // - デフォルト引数と分割代入を使用して実装してください。
 async function fetchUserData(userId, options = {}) {
   // TODO: デフォルト引数と分割代入を活用して options を処理
+  const { headers = {}, timeout = 5000 } = options;
   // TODO: fetch を使って API にアクセス
+  const url = `https://api.example.com/users/${userId}`;
+  const controller = new AbortController();
+  const timer = setTimeout(() => controller.abort(), timeout);
   // TODO: 成功時は JSON データを返す
   // TODO: エラー時は console.error にメッセージを出力し、undefined を返す
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers,
+      signal: controller.signal,
+    });
+
+    clearTimeout(timer);
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      console.error(`${response.status}`);
+      return undefined;
+    }
+  } catch (error) {
+    console.error(error.message);
+    return undefined;
+  }
 }
-
-
+const user = await fetchUserData("1");
+console.log(user);
 
 // TODO 2: データ変換用のユーティリティ関数群を含むオブジェクト transformers を実装してください。
 // snake_case を camelCase に変換する関数と、日付を整形する関数を含めてください。
+
+function snakeToCamel(str) {
+  return str.replace(/_([a-z])/g, (_, char) => char.toUpperCase());
+}
+
 const transformers = {
   // TODO: オブジェクトのキーを snake_case から camelCase に変換する関数 snakeToCamel を実装
   snakeToCamel: (obj) => {
-    // 例: { user_id: 1, user_name: 'Taro' } → { userId: 1, userName: 'Taro' }
-  },
+    // Array.はjsの組み込みオブジェクト。クラス。
+    if (Array.isArray(obj)) {
+      // map()は配列の各要素に対して()の中の関数を適用して、新しい配列を作る。
+      return obj.map(transformers.snakeToCamel);
+    } else if (obj !== null && typeof obj === 'object') {
+      return Object.keys(obj).reduce((acc, key) => {
+      const newKey = snakeToCamel(key);
+      // ↓これが再帰関数の部分で、再度関数を呼びだすことによって、ネストが深いところまで、処理を行うことができる。
+      acc[newKey] = transformers.snakeToCamel(obj[key]);
+      return acc;
+    }, {});
+  } else{
+    return obj;
+  }
+}};
 
-  // TODO: 日付を 'YYYY-MM-DD' または 'YYYY/MM/DD' 形式で返す formatDate 関数を実装
-  formatDate: (date, format = 'YYYY-MM-DD') => {
-    // 例: formatDate(new Date('2024-06-01')) → '2024-06-01'
+const data = {
+  user_id: 1,
+  user_name: 'Taro',
+  profile_data: {
+    birth_date: '2000-01-01'
   }
 };
+
+console.log(transformers.snakeToCamel(data));
+
+
+// TODO: 日付を 'YYYY-MM-DD' または 'YYYY/MM/DD' 形式で返す formatDate 関数を実装
+const formatDate = (date, format = "YYYY-MM-DD") => {
+  // 例: formatDate(new Date('2024-06-01')) → '2024-06-01'
+  const year = date.getFullYear();
+  const month = String(date.getMonth()+1).padStart(2,'0');
+  const day = String(date.getDate()).padStart(2,'0');
+  if (format === 'YYYY-MM-DD'){
+    return `${year}-${month}-${day}`;
+  } else {
+    return `${year}/${month}/${day}`;
+  }
+};
+console.log(formatDate(new Date('2024-06-01')));
