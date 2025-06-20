922703d887253675442d1d86a12d4f95eb9a3b43
otsuki <rena141209.2002@gmail.com>
2025-06-20 12:15:06 +0900

    work9の課題完了

--- src/02.実務応用とVue、TypeScript統合/work9.vue
+++ src/02.実務応用とVue、TypeScript統合/work9.vue
@@ -18,28 +18,42 @@
 
     <!-- TODO 1: ローディング中は「読み込み中...」と表示 -->
     <!-- 例: v-if="loading" -->
+    <div v-if = "loading">読み込み中...</div>
 
     <!-- TODO 2: エラーがあればエラーメッセージを表示 -->
     <!-- 例: v-if="error" -->
+    <div v-if = "error">{{ error }}</div>
 
     <!-- TODO 3: ユーザーリストを表示 -->
     <!-- 例: v-for で users を表示 -->
+    <div v-for = "user in users" :key="user.id" class="user-list">{{ user.name }}</div>
 
     <!-- TODO 4 (応用): adminUsers を別でフィルターして表示してもOK -->
-    
+    <div v-for = "admin in adminUsers" :key="admin.id" class="user-list">{{ admin.name }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue';
-import { useUser } from '@/composables/useUser';
+// import { useUser } from '@/composables/useUser';
+import { useUser } from 'src/02.実務応用とVue、TypeScript統合/work8.ts';
 
 // TODO 5: useUser() composable を呼び出して、必要な状態・関数を取得してください
 // - users, loading, error, fetchUsers など
+const { users, loading, error, adminUsers, fetchUsers } = useUser();
+
 
 // TODO 6: onMounted フックを使って、コンポーネントマウント時に fetchUsers() を実行してください
+onMounted ( () =>{
+  fetchUsers();
+});
 </script>
 
 <style scoped>
 /* TODO: スタイルは自由に実装してください */
+.user-list {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
 </style>
